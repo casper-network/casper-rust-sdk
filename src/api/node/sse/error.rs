c309f6eb93ca03defff9dc10119293b84ffff5ce
sczembor <stanislaw.czembor@gmail.com>
2024-07-31 14:27:07 +0200

    remove unused error declarations

--- src/api/node/sse/error.rs
+++ src/api/node/sse/error.rs
@@ -2,14 +2,10 @@ use super::{types::CoreCommand, SseData};
 use eventsource_stream::EventStreamError;
 use thiserror::Error;
 
-//TODO: after implementing proper mock sse revisit the errors below and remove the unnecessary ones
 #[derive(Error, Debug)]
 pub enum ClientError {
     #[error("Failed to connect to SSE endpoint: {0}")]
     ConnectionError(#[from] reqwest::Error),
-
-    #[error("Not connected to event stream")]
-    NotConnected,
 
     #[error("SSE stream exhausted unexpectedly")]
     StreamExhausted,
@@ -26,12 +22,6 @@ pub enum ClientError {
     #[error("Node shutdown")]
     NodeShutdown,
 
-    #[error("Timeout while waiting for event")]
-    Timeout,
-
-    #[error("Invalid command received")]
-    InvalidCommand,
-
     #[error("Failed to send command to core: {0}")]
     CommandSendError(#[from] tokio::sync::mpsc::error::SendError<CoreCommand>),
 
@@ -40,12 +30,6 @@ pub enum ClientError {
 
     #[error("Failed to recive command from core: {0}")]
     CommandRecvError(#[from] tokio::sync::oneshot::error::RecvError),
-
-    #[error("Event handler error")]
-    EventHandlerError(#[from] Box<dyn std::error::Error + Send + Sync>),
-
-    #[error("Error registering event handler")]
-    RegisterEventHanbdlerError,
 
     #[error("Failed to send Event into the channel: {0}")]
     ChannelInternalError(#[from] tokio::sync::mpsc::error::TrySendError<SseData>),
