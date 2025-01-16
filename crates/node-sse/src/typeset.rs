9ac4ad5f3977c1a9f0388f5bf0d821d0dabe7300
Mark A. Greenslade <asladeofgreen@gmail.com>
2025-01-16 12:11:04 +0100

    1. Reinitialising typeset.

---
+++ crates/node-sse/src/typeset.rs
@@ -0,0 +1,45 @@
+// use serde::{Deserialize, Serialize};
+
+// ------------------------------------------------------------------------
+// Declarations.
+// ------------------------------------------------------------------------
+
+// #[derive(Clone, Copy, Eq, PartialEq, Debug, Hash)]
+// pub enum EventType {
+//     ApiVersion,
+//     SidecarVersion,
+//     BlockAdded,
+//     TransactionAccepted,
+//     TransactionProcessed,
+//     TransactionExpired,
+//     Fault,
+//     FinalitySignature,
+//     Step,
+//     Shutdown,
+// }
+
+// #[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
+// pub enum SseData {
+//     ApiVersion(serde_json::Value),
+//     SidecarVersion(serde_json::Value),
+//     BlockAdded(serde_json::Value),
+//     TransactionAccepted(serde_json::Value),
+//     TransactionProcessed(serde_json::Value),
+//     TransactionExpired(serde_json::Value),
+//     Fault(serde_json::Value),
+//     FinalitySignature(serde_json::Value),
+//     Step(serde_json::Value),
+//     Shutdown,
+// }
+
+// ------------------------------------------------------------------------
+// Constructors.
+// ------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------
+// Accessors.
+// ------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------
+// Methods.
+// ------------------------------------------------------------------------
